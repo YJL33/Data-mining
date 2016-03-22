0eb8824df8ed9340bd30b3b0a34822e0b872d321
YJL33 <yunjun.l33@gmail.com>
2016-03-21 21:49:07 -0700

    Sort the output as required format

--- Assignment 3/yunjun_lee_recommendation.py
+++ Assignment 3/yunjun_lee_recommendation.py
@@ -4,18 +4,19 @@ Assignment 3
 Problem 2: Making Recommendations
 
 Detail: https://goo.gl/FVfN7k
+Debug: remove # at line 81
 """
 import sys
 import json
 
 def recommend(inputdata_record, inputdata_lsh):
     """
-    inputdata1: Distinct User Records, e.g. ["Rachel", [1, 2, 3, 4]] ["Monica", [2, 3, 4, 5]]
-    inputdata2: candidates from LSH, e.g. ["U4", "U8"] ["U35", "U8"]
+    inputdata_record: Distinct User Records, e.g. ["Rachel", [1, 2, 3, 4]] ["Monica", [2, 3, 4, 5]]
+    inputdata_lsh: candidates from LSH, e.g. ["U4", "U8"] ["U35", "U8"]
 
     Steps:
     1. For each distinct user, find all similar users based on LSH output, by findalikeuser()
-    2. For those users have more than 5 similar ones, compare the similarity, by findtopfive()
+    2. If number of similar users >= 5, compare the Jaccard similarity in record, by findtopfive()
     3. Make recommendations for each user based on their similar counterparts, by endorsement()
     """
 
@@ -43,15 +44,15 @@ def recommend(inputdata_record, inputdata_lsh):
         for user, alike_users in sim_usrs.iteritems():
             if len(alike_users) > 5:
                 similarities = []
-                for cand in alike_users:   # Calculate Jaccard Similarity (common/all)
+                for cand in alike_users:   # Calculate Jaccard Similarities (common/all)
                     common_element = float(len(list(set(rec[user]) & set(rec[cand]))))
                     all_element = float(len(list(set(rec[user]) | set(rec[cand]))))
                     similarities.append([common_element/all_element, cand])
                 similarities.sort(reverse=True)
-                last = 4                   # Index of last element in top five
+                last = 4                   # Index of last element in top-five (or more)
                 while similarities[last][0] == similarities[last+1][0]:
                     last += 1
-                for i in xrange(last+1, len(alike_users)):
+                for i in xrange(last+1, len(alike_users)):  # Remove un-desired elements
                     alike_users.remove(similarities[i][1])
         return sim_usrs
 
@@ -60,35 +61,34 @@ def recommend(inputdata_record, inputdata_lsh):
         input: rec = user experience (record), sim_usrs = output from findalikeuser()
         output: a list containing users and recommended movies.
         """
-        recommendations = []
-        for user in sim_usrs.keys():          # For each user,
-            rec_movies = {}                   # Count the unseen movies by
-            for sim_usr in sim_usrs[user]:    # checking each similar users,
-                for movie in rec[sim_usr]:     # and count each movie that similar user has seen
-                    if movie not in rec[user]:
-                        if movie not in rec_movies:
-                            rec_movies[movie] = 1
+        recommendations = []            # Recommendation Movie List for all user
+        for user in sim_usrs.keys():            # For each user, 
+            rec_movies = {}       # build a new dictionary, Key = unseen movie, Values = view count
+            for sim_usr in sim_usrs[user]:      # check each of his/her similar user
+                for movie in rec[sim_usr]:      # check each movie that similar user has seen
+                    if movie not in rec[user]:  # an unseen movie
+                        if movie not in rec_movies:   # Count it
+                            rec_movies[movie] = 1       # First appear
                         else:
-                            rec_movies[movie] += 1
-            rec_list = []
-            rec = False
+                            rec_movies[movie] += 1      # Count++
+            # Now determine which movie should be recommended
+            rec_list = []               # Recommendation Movie List for single user
             for movie in rec_movies:
-                if rec_movies[movie] >= 3:
-                    rec_list.append(movie)
-                    rec = True
-            if rec:
+                if rec_movies[movie] >= 3:      # Criteria is given by assignment:
+                    rec_list.append(movie)      # movie must be watched by at least 3 users
+            if len(rec_list) > 0:
                 recommendations.append([user, rec_list])
-        #print recommendations
-        return recommendations
+        #print sorted(recommendations)
+        return sorted(recommendations)
 
-# Below this line are main part.
-    user_exp = {}
-    for line in inputdata_record:          # Read each line in inputdata_record
+# Below this line are the main part.
+    user_exp = {}                           # All user experience
+    for line in inputdata_record:           # Read each line in inputdata_record
         user_exp[(json.loads(line)[0].encode("utf-8"))] = json.loads(line)[1]
 
-    similar_users = findalikeuser(inputdata_lsh)
-    top_five_list = findtopfive(user_exp, similar_users)
-    recommendations = endorsement(user_exp, top_five_list)
+    similar_users = findalikeuser(inputdata_lsh)            # Step 1
+    top_five_list = findtopfive(user_exp, similar_users)    # Step 2
+    recommendations = endorsement(user_exp, top_five_list)  # Step 3
 
     return recommendations
 # Do not modify below this line
